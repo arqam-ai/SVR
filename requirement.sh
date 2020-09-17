1d0752571e6e5c8c64135e265a61e53299be74d2
Yefan <39088217+YefanZhou@users.noreply.github.com>
2020-09-17 19:35:55 +0800

    w

---
+++ requirement.sh
@@ -0,0 +1,12 @@
+pip install opencv-python \
+            argparse \
+            time \
+            yaml \
+            numpy \
+            matplotlib \
+            matplotlib-base \
+            scikit-image \
+            scipy \
+            tensorboardx \
+            tqdm \
+            trimesh \
