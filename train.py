9d4a91b9b3d3ad90dfdb6304d37eb29ea7bfe227
Yefan <39088217+YefanZhou@users.noreply.github.com>
2020-09-17 18:13:32 +0800

    download environ

--- train.py
+++ train.py
@@ -4,14 +4,11 @@ Training stage for auto encoder
 import os
 import sys
 import argparse
-import glog as logger
 import logging
-import open3d as o3d
 import torch
 import torch.utils.data
 import torch.nn as nn
 import torch.autograd
-import optparse
 import time
 from dataset.dataset import what3d_dataset_views
 from model.generator_res_Folding import GeneratorVanilla
@@ -148,193 +145,193 @@ def main(args):
 
 
 if __name__ == "__main__":
-    parser = optparse.OptionParser(sys.argv[0], description="Training Encoder_decoder")
+    parser = argparse.ArgumentParser()(sys.argv[0], description="Training Encoder_decoder")
 
     # dataset info
-    parser.add_option("--ptcloud-path", dest="ptcloud_path", type=str,   default="ptcloud_0.npz",                  help='path of the ptcloud')
-    parser.add_option("--img-path",     dest="img_path",     type=str,   default='renderings',                   help='path of the image')
-    parser.add_option("--label-path",   dest="label_path",   type=str,   default='label.npz',                    help='path of the image')
-    parser.add_option("--data-basedir", dest="data_basedir", type=str,   default='/home/../../public/zyf/What3D',help='path of the data folder') 
-    parser.add_option("--splits-path",  dest="splits_path",  type=str,   default='splits',help='path of the data folder') 
-    parser.add_option("--class-path",   dest="class_path",   type=str,   default='classes.txt',help="class name list")
-    parser.add_option("--image-size",   dest="image_size",   type=int,   default = 224,help="image size for network")
-    parser.add_option("--sample-ratio", dest="sample_ratio", type=float, default = 0.001,help="ratio to sample the dataset")
-    parser.add_option("--views",dest="views", type=str,default= '0',help="five view for each instance")
-    parser.add_option("--pts-num",dest="pts_num", type=int,default=1024,help="number of points in a ptcloud")
+    parser.add_argument("--ptcloud-path", dest="ptcloud_path", type=str,   default="ptcloud_0.npz",                  help='path of the ptcloud')
+    parser.add_argument("--img-path",     dest="img_path",     type=str,   default='renderings',                   help='path of the image')
+    parser.add_argument("--label-path",   dest="label_path",   type=str,   default='label.npz',                    help='path of the image')
+    parser.add_argument("--data-basedir", dest="data_basedir", type=str,   default='/home/../../public/zyf/What3D',help='path of the data folder') 
+    parser.add_argument("--splits-path",  dest="splits_path",  type=str,   default='splits',help='path of the data folder') 
+    parser.add_argument("--class-path",   dest="class_path",   type=str,   default='classes.txt',help="class name list")
+    parser.add_argument("--image-size",   dest="image_size",   type=int,   default = 224,help="image size for network")
+    parser.add_argument("--sample-ratio", dest="sample_ratio", type=float, default = 0.001,help="ratio to sample the dataset")
+    parser.add_argument("--views",dest="views", type=str,default= '0',help="five view for each instance")
+    parser.add_argument("--pts-num",dest="pts_num", type=int,default=1024,help="number of points in a ptcloud")
 
-    parser.add_option("--mode",
+    parser.add_argument("--mode",
                       dest="mode", type=str,
                       default="object", 
                       help="['viewer', 'object']")
 
     # Log file info
-    parser.add_option("--log-dir",
+    parser.add_argument("--log-dir",
                       dest="log_dir", type=str,
                       default="logs/",
                       help="log folder to save training stats as numpy files")
 
-    parser.add_option("--log-filename", type=str,
+    parser.add_argument("--log-filename", type=str,
                       dest="log_filename",
                       default="Train.log",
                       help="Name of log file.")
 
-    parser.add_option("--output-dir",
+    parser.add_argument("--output-dir",
                       dest="output_dir", type=str,
                       default='results/',
                       help='result folder to save generated ptc during training')
 
-    parser.add_option("--snapshot-dir",
+    parser.add_argument("--snapshot-dir",
                       dest="snapshot_dir", type=str,
                       default='snapshots/',
                       help='snapshot folder to save training checkpoint')
 
-    parser.add_option('--verbose_per_n_batch',
+    parser.add_argument('--verbose_per_n_batch',
                      dest="verbose_per_n_batch", type=int,
                      default= 5,
                      help='log training stats to console every n batch (<=0 disables training log)')
 
-    parser.add_option('--test_per_n_epoch',
+    parser.add_argument('--test_per_n_epoch',
                      dest="test_per_n_epoch", type=int,
                      default= 1,
                      help='test per epoch')                 
 
     ## training parameter
-    parser.add_option("--model",
+    parser.add_argument("--model",
                     dest="model", type=str,
                     default="foldingnet",
                     help=['foldingnet','psgn'])
                     
-    parser.add_option("--use-manifold",
+    parser.add_argument("--use-manifold",
                       dest="use_manifold", action="store_true",
                       default=False,
                       help='whether to use manifold learning')
 
-    parser.add_option("--alpha",
+    parser.add_argument("--alpha",
                       dest="alpha", type=float,
                       default= 0.,
                       help='mixup interpolation strength (uniform=1., ERM=0.)')
 
-    parser.add_option("--total-epochs",
+    parser.add_argument("--total-epochs",
                       dest="total_epochs", type=int,
                       default=300,
                       help='training epochs')
     
-    parser.add_option("--train-batch-size",
+    parser.add_argument("--train-batch-size",
                       dest="train_batch_size", type=int,
                       default=128,
                     help='training batch size')
-    parser.add_option("--test-batch-size",
+    parser.add_argument("--test-batch-size",
                       dest="test_batch_size", type=int,
                       default=200,
                     help='testing batch size')
-    parser.add_option("--val-batch-size",
+    parser.add_argument("--val-batch-size",
                       dest="val_batch_size", type=int,
                       default=200,
                     help='val batch size')
-    parser.add_option("--lr-G",
+    parser.add_argument("--lr-G",
                       dest="lr_G", type=float,
                       default=1e-4,
                     help='learning rate')
-    parser.add_option("--lr-D",
+    parser.add_argument("--lr-D",
                       dest="lr_D", type=float, default=1e-4,
                       help='learning rate')
-    parser.add_option("--pool-size",
+    parser.add_argument("--pool-size",
                       dest="pool_size", type=int,
                       default=0,
                       help='history cache for training discriminator')
-    parser.add_option("--G-adam-beta1",
+    parser.add_argument("--G-adam-beta1",
                       dest="G_adam_beta1", type=float,
                       default=0.5,
                       help="beta1 for Adam optimizer (G)")
-    parser.add_option("--mean",
+    parser.add_argument("--mean",
                       dest="mean", type=float,
                       default=0.0,
                       help="value of mean for noise (Gaussian)")
-    parser.add_option("--stddev",
+    parser.add_argument("--stddev",
                       dest="stddev", type=float,
                       default=1.0,
                       help="value of stddev for noise (Gaussian)")
 
-    parser.add_option("--lambda-loss-primitive",
+    parser.add_argument("--lambda-loss-primitive",
                       dest="lambda_loss_primitive", type=float,
                       default=1.0,
                       help="lambda for point cloud first generation")
 
-    parser.add_option("--lambda-loss-fine",
+    parser.add_argument("--lambda-loss-fine",
                       dest="lambda_loss_fine", type=float,
                       default=1.0,
                       help="lambda for point cloud second generation")
 
-    parser.add_option("--lambda-loss-manifold",
+    parser.add_argument("--lambda-loss-manifold",
                       dest="lambda_loss_manifold", type=float,
                       default= 1.0,
                       help="lambda for codeword manifold")
 
-    parser.add_option("--lambda-loss-classification",
+    parser.add_argument("--lambda-loss-classification",
                       dest="lambda_loss_classification", type=float,
                       default= 1.0,
                       help="lambda for img classification")
 
-    parser.add_option('--momentum',type=float,
+    parser.add_argument('--momentum',type=float,
                       default= 0.9,
                      help='Solver momentum')
-    parser.add_option("--weight-decay",
+    parser.add_argument("--weight-decay",
                       dest="weight_decay", type=float,
                       default= 1e-6,
                     help='weight decay')
-    parser.add_option('--lr_decay_step',
+    parser.add_argument('--lr_decay_step',
                       type=int, default=30,
                       help='learning rate decay step'
                       )
-    parser.add_option('--lr_decay_rate',
+    parser.add_argument('--lr_decay_rate',
                       type=float, default=0.1,
                       help='learning rate decay rate'
                       )
-    parser.add_option("--tensorboard", action="store_true",
+    parser.add_argument("--tensorboard", action="store_true",
                       dest="tensorboard",
                       default= None) 
 
-    parser.add_option("--checkpoint-model", type="str",
+    parser.add_argument("--checkpoint-model", type="str",
                       dest="checkpoint_model",
                       #default= "snapshots/model_train_best.pth",
                       default= None,
                       help="Path to pretrained model G net")
 
-    parser.add_option("--checkpoint-solver", type="str",
+    parser.add_argument("--checkpoint-solver", type="str",
                       dest="checkpoint_solver",
                       #default= "snapshots/solver_train_best.pth",
                       default= None,
                       help="Path to pretrained solver")
 
-    parser.add_option("--save-results", action = "store_true",
+    parser.add_argument("--save-results", action = "store_true",
                       dest="save_results",
                       default = False,
                       help = "save the results in evaluation test")
 
-    parser.add_option("--train", action="store_true",
+    parser.add_argument("--train", action="store_true",
                       dest="train",
                       default= False,
                       help="run training", )
 
-    parser.add_option("--test", action="store_true",
+    parser.add_argument("--test", action="store_true",
                       dest="test",
                       default= False,
                       help="run testing (generate ptcloud)", )
 
-    parser.add_option("--read-view", action="store_true",
+    parser.add_argument("--read-view", action="store_true",
                       dest="read_view",
                       default= False,
                       help="if adding the view information in training", )
 
-    parser.add_option("--folding-twice", action="store_true",
+    parser.add_argument("--folding-twice", action="store_true",
                       dest="folding_twice",
                       default= False,
                       help="if folding twice", )
 
-    parser.add_option("--if-BNstats", action="store_true", dest="if_BNstats", default=False, help="if calculate bn stats")
-    parser.add_option("--bnstats-step", type=int, dest="bnstats_step", default=30, help="step to log batch norm stats")
+    parser.add_argument("--if-BNstats", action="store_true", dest="if_BNstats", default=False, help="if calculate bn stats")
+    parser.add_argument("--bnstats-step", type=int, dest="bnstats_step", default=30, help="step to log batch norm stats")
     
-    (args, opts) = parser.parse_args()
+    args = parser.parse_args()
    
     args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     args.script_folder = os.path.dirname(os.path.abspath(__file__))
