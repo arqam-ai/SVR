abc06d259d639e5bb864d6b24f8e58c038dc2440
Yefan <39088217+YefanZhou@users.noreply.github.com>
2020-10-05 19:02:55 +0800

    update sh

--- experiment/viewer_models_v2/viewer_ucb.sh
+++ experiment/viewer_models_v2/viewer_ucb.sh
@@ -14,43 +14,44 @@ BNSTATS_STEP=20                        # step to do batch norm stats
 TEST_STEP=10                           # step to do test
 NUM_WORKER=4                 
 # CUDA 
-CUDA1=0                                # gpu id for model1  foldingres no BN hidden 6 
+CUDA1=3                                # gpu id for model1  foldingres no BN hidden 6 
 CUDA2=0                                # gpu id for model1  foldingres BN hidden 6 
 CUDA3=0                                # gpu id for model1  batch size = 32 foldingres BN hidden 6
 
 
-# # model1  foldingres no BN hidden 6 batch size = 64
-# CUDA_VISIBLE_DEVICES=$CUDA1 python ../../train.py \
-#                           --data-basedir '../../../What3D' \
-# 						  --ptcloud-path "ptcloud_n.npz" \
-#              		      --model "foldingres"      `# model Info`\
-#                           --SVR \
-#                           --num_layers 6 \
-#                           --hidden_neurons 512 \
-#                           --bottleneck_size 512 \
-#                           --remove_all_batchNorms \
-#                           --mode $MODE             `# dataset Info`\
-# 						  --image-size 224 \
-# 						  --view $VIEWS \
-# 						  --sample-ratio $SAMPLERATIO \
-#                           --num-worker $NUM_WORKER \
-# 						  --total-epoch $EPOCH      `# training Info`\
-# 						  --train-batch-size $TRAINBATCH \
-# 						  --test-batch-size $TESTBATCH \
-# 						  --val-batch-size $VALBATCH \
-# 						  --verbose_per_n_batch 50 \
-# 						  --test_per_n_epoch $TEST_STEP \
-# 						  --lambda-loss-fine 1. \
-# 						  --lambda-loss-primitive 1. \
-#                           --lr-G $LR \
-#                           --momentum $MOMENTUM \
-#                           --lr_decay_step $DECAYSTEP \
-#                           --weight-decay $WEIGHTDECAY \
-# 						  --log-dir	'foldingres6_FC/'  `# logging Info`\
-# 						  --tensorboard \
-# 						  --save-results \
-# 						  --test \
-# 						  --train & 
+CUDA_VISIBLE_DEVICES=$CUDA1 python ../../train.py \
+                          --data-basedir '../../../What3D' \
+						  --ptcloud-path "ptcloud_n.npz" \
+             		      --model "atlasnet"      `# model Info`\
+                          --SVR \
+                          --num_layers 6 \
+                          --hidden_neurons 512 \
+                          --bottleneck_size 512 \
+                          `# --remove_all_batchNorms`\
+						  --nb_primitives 4       `# atlasnet Info 1 4 16`\
+ 						  --template_type "SQUARE" \
+                          --mode $MODE             `# dataset Info`\
+						  --image-size 224 \
+						  --view $VIEWS \
+						  --sample-ratio $SAMPLERATIO \
+                          --num-worker $NUM_WORKER \
+						  --total-epoch $EPOCH      `# training Info`\
+						  --train-batch-size $TRAINBATCH \
+						  --test-batch-size $TESTBATCH \
+						  --val-batch-size $VALBATCH \
+						  --verbose_per_n_batch 50 \
+						  --test_per_n_epoch $TEST_STEP \
+						  --lambda-loss-fine 1. \
+						  --lambda-loss-primitive 1. \
+                          --lr-G $LR \
+                          --momentum $MOMENTUM \
+                          --lr_decay_step $DECAYSTEP \
+                          --weight-decay $WEIGHTDECAY \
+						  --log-dir	'atlasnet_viewer_4square_lr6_random_ucb/'  `# logging Info`\
+						  --tensorboard \
+						  --save-results \
+						  --test \
+						  --train 
 
 # # model2  foldingres BN hidden 6 batch size = 64
 # CUDA_VISIBLE_DEVICES=$CUDA2 python ../../train.py \
@@ -85,36 +86,36 @@ CUDA3=0                                # gpu id for model1  batch size = 32 fold
 # 						  --test \
 # 						  --train &
 
-# model3  foldingres BN hidden 6 batch size = 32
-CUDA_VISIBLE_DEVICES=$CUDA3 python ../../train.py \
-                          --data-basedir '../../../What3D' \
-						  --ptcloud-path "ptcloud_n.npz" \
-             		      --model "foldingres"      `# model Info`\
-                          --SVR \
-                          --num_layers 6 \
-                          --hidden_neurons 512 \
-                          --bottleneck_size 512 \
-						  `# --remove_all_batchNorms`\
-                          --mode $MODE             `# dataset Info`\
-						  --image-size 224 \
-						  --view $VIEWS \
-						  --sample-ratio $SAMPLERATIO \
-                          --num-worker $NUM_WORKER \
-						  --total-epoch $EPOCH      `# training Info`\
-						  `#--train-batch-size $TRAINBATCH`\
-						  --train-batch-size 32 \
-						  --test-batch-size $TESTBATCH \
-						  --val-batch-size $VALBATCH \
-						  --verbose_per_n_batch 50 \
-						  --test_per_n_epoch $TEST_STEP \
-						  --lambda-loss-fine 1. \
-						  --lambda-loss-primitive 1. \
-                          --lr-G $LR \
-                          --momentum $MOMENTUM \
-                          --lr_decay_step $DECAYSTEP \
-                          --weight-decay $WEIGHTDECAY \
-						  --log-dir	'foldingres6_FCBN_BS32/'  `# logging Info`\
-						  --tensorboard \
-						  --save-results \
-						  --test \
-						  --train &
+# # model3  foldingres BN hidden 6 batch size = 32
+# CUDA_VISIBLE_DEVICES=$CUDA3 python ../../train.py \
+#                           --data-basedir '../../../What3D' \
+# 						  --ptcloud-path "ptcloud_n.npz" \
+#              		      --model "foldingres"      `# model Info`\
+#                           --SVR \
+#                           --num_layers 6 \
+#                           --hidden_neurons 512 \
+#                           --bottleneck_size 512 \
+# 						  `# --remove_all_batchNorms`\
+#                           --mode $MODE             `# dataset Info`\
+# 						  --image-size 224 \
+# 						  --view $VIEWS \
+# 						  --sample-ratio $SAMPLERATIO \
+#                           --num-worker $NUM_WORKER \
+# 						  --total-epoch $EPOCH      `# training Info`\
+# 						  `#--train-batch-size $TRAINBATCH`\
+# 						  --train-batch-size 32 \
+# 						  --test-batch-size $TESTBATCH \
+# 						  --val-batch-size $VALBATCH \
+# 						  --verbose_per_n_batch 50 \
+# 						  --test_per_n_epoch $TEST_STEP \
+# 						  --lambda-loss-fine 1. \
+# 						  --lambda-loss-primitive 1. \
+#                           --lr-G $LR \
+#                           --momentum $MOMENTUM \
+#                           --lr_decay_step $DECAYSTEP \
+#                           --weight-decay $WEIGHTDECAY \
+# 						  --log-dir	'foldingres6_FCBN_BS32/'  `# logging Info`\
+# 						  --tensorboard \
+# 						  --save-results \
+# 						  --test \
+# 						  --train &
--- experiment/viewer_models_v2_seu/viewer_seu.sh
+++ experiment/viewer_models_v2_seu/viewer_seu.sh
@@ -18,7 +18,7 @@ CUDA1=1                                # gpu id for model1  foldingres no BN hid
 CUDA2=0                                # gpu id for model2  foldingres BN hidden 6 
 CUDA3=0                                # gpu id for model3  batch size = 32 foldingres BN hidden 6
 CUDA4=1 							   # gpu id for model4  atlasnet
-
+CUDA5=0								   # gpu id for model5  atlasnet 4 square 
 # # model1  foldingres no BN hidden 6 batch size = 64
 # CUDA_VISIBLE_DEVICES=$CUDA1 python ../../train.py \
 #                           --data-basedir '../../../What3D' \
@@ -119,16 +119,50 @@ CUDA4=1 							   # gpu id for model4  atlasnet
 # 						  --test \
 # 						  --train &
 
-CUDA_VISIBLE_DEVICES=$CUDA4 python ../../train.py \
+# CUDA_VISIBLE_DEVICES=$CUDA4 python ../../train.py \
+#                           --data-basedir '../../../What3D' \
+# 						  --ptcloud-path "ptcloud_n.npz" \
+#              		      --model "atlasnet"      `# model Info`\
+#                           --SVR \
+#                           --num_layers 6 \
+#                           --hidden_neurons 512 \
+#                           --bottleneck_size 512 \
+#                           `# --remove_all_batchNorms`\
+# 						  --nb_primitives 1       `# atlasnet Info 1 4 16`\
+#  						  --template_type "SQUARE" \
+#                           --mode $MODE             `# dataset Info`\
+# 						  --image-size 224 \
+# 						  --view $VIEWS \
+# 						  --sample-ratio $SAMPLERATIO \
+#                           --num-worker $NUM_WORKER \
+# 						  --total-epoch $EPOCH      `# training Info`\
+# 						  --train-batch-size $TRAINBATCH \
+# 						  --test-batch-size $TESTBATCH \
+# 						  --val-batch-size $VALBATCH \
+# 						  --verbose_per_n_batch 50 \
+# 						  --test_per_n_epoch $TEST_STEP \
+# 						  --lambda-loss-fine 1. \
+# 						  --lambda-loss-primitive 1. \
+#                           --lr-G $LR \
+#                           --momentum $MOMENTUM \
+#                           --lr_decay_step $DECAYSTEP \
+#                           --weight-decay $WEIGHTDECAY \
+# 						  --log-dir	'atlasnet_viewer_1square/'  `# logging Info`\
+# 						  --tensorboard \
+# 						  --save-results \
+# 						  --test \
+# 						  --train &
+
+CUDA_VISIBLE_DEVICES=$CUDA5 python ../../train.py \
                           --data-basedir '../../../What3D' \
 						  --ptcloud-path "ptcloud_n.npz" \
              		      --model "atlasnet"      `# model Info`\
                           --SVR \
-                          --num_layers 6 \
+                          --num_layers 2 \
                           --hidden_neurons 512 \
                           --bottleneck_size 512 \
                           `# --remove_all_batchNorms`\
-						  --nb_primitives 1       `# atlasnet Info 1 4 16`\
+						  --nb_primitives 4       `# atlasnet Info 1 4 16`\
  						  --template_type "SQUARE" \
                           --mode $MODE             `# dataset Info`\
 						  --image-size 224 \
@@ -147,7 +181,7 @@ CUDA_VISIBLE_DEVICES=$CUDA4 python ../../train.py \
                           --momentum $MOMENTUM \
                           --lr_decay_step $DECAYSTEP \
                           --weight-decay $WEIGHTDECAY \
-						  --log-dir	'atlasnet_viewer_1square/'  `# logging Info`\
+						  --log-dir	'atlasnet_viewer_4square_lr2_random_seu/'  `# logging Info`\
 						  --tensorboard \
 						  --save-results \
 						  --test \
